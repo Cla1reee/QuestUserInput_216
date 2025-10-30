3210f7eadeb620f8ee1c83b1cfb6e08448f0f611
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-30 08:51:13 +0700

    feat: Add FormDataDiri composable stub
    Introduced a new composable function `FormDataDiri` to manage user personal data input.
    
    This initial implementation includes:
    - The basic structure of the `@Composable` function.
    - State variables using `mutableStateOf` for name (`textNama`, `nama`), address (`textAlamat`, `alamat`), and gender (`textJK`, `jenis`).
    - A static list of gender options (`gender`).

--- app/src/main/java/com/example/inputpengguna/TextEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TextEdit.kt
@@ -1,2 +1,15 @@
 package com.example.inputpengguna
 
+@Composable
+fun FormDataDiri(modifer: Modifier
+){
+    var textNama by remember { mutableStateOf(value"") }
+    var textAlamat by remember { mutableStateOf(value"") }
+    var textJK by remember { mutableStateOf(value"") }
+
+    var nama by remember { mutableStateOf(value"") }
+    var alamat by remember { mutableStateOf(value"") }
+    var jenis by remember { mutableStateOf(value"") }
+
+    val gender:List<string> = listOf("Laki-laki","Perempuan")
+}
