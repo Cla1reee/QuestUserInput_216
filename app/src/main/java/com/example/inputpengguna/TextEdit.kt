7730037db15c3c3d97369a9def2d34ce297657b6
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-30 09:52:18 +0700

    refactor: Fix package name and imports
    This commit corrects the package name in `TextEdit.kt` from `com.example.inputpenggunaimport` to `com.example.InputPenggunaimport.R`.
    
    - Removed an extraneous, non-functional `androidx.compose.foundation.layout.Arrangement` import statement.

--- app/src/main/java/com/example/inputpengguna/TextEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TextEdit.kt
@@ -1,4 +1,4 @@
-package com.example.InputPenggunaimport.R
+package com.example.inputpengguna
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.ui.res.dimensionResource
@@ -24,7 +24,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
